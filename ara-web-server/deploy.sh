f4c5423a660e56bb7e25dbcbd9d297b55a31b84a
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2019-04-05 00:39:47 +0530

    [ara-web-server] Update deployment script and supervisor conf

--- ara-web-server/deploy.sh
+++ ara-web-server/deploy.sh
@@ -6,7 +6,7 @@ echo "Executing deploy.sh" >> /home/aravinth/dreamspace.academy/deploy.log
 echo "Pulling repo" >> /home/aravinth/dreamspace.academy/deploy.log
 cd /home/aravinth/dreamspace.academy/repo/ && git pull
 echo "Removing old html" >> /home/aravinth/dreamspace.academy/deploy.log
-rm -r /home/aravinth/dreamspace.academy/html
+rm -rf /home/aravinth/dreamspace.academy/html
 mkdir /home/aravinth/dreamspace.academy/html
 echo "Creating new html" >> /home/aravinth/dreamspace.academy/deploy.log
-cp -r /home/aravinth/dreamspace.academy/repo/* /home/aravinth/dreamspace.academy/html/
+cp -rf /home/aravinth/dreamspace.academy/repo/* /home/aravinth/dreamspace.academy/html/
