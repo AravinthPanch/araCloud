8c663fdd334915166afc39c45e2f07726a5a9f1f
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2019-07-04 13:59:32 +0200

    Add aracreate.comd deployment scripts

---
+++ deploy-aracreate-com.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#TODO: Parameterize links, check files before operations
+
+echo "Executing deploy.sh" >> /home/aravinth/aracreate.com/deploy.log
+echo "Pulling repo" >> /home/aravinth/aracreate.com/deploy.log
+cd /home/aravinth/aracreate.com/repo/ && git pull
+echo "Removing old html" >> /home/aravinth/aracreate.com/deploy.log
+rm -rf /home/aravinth/aracreate.com/html
+mkdir /home/aravinth/aracreate.com/html
+echo "Creating new html" >> /home/aravinth/aracreate.com/deploy.log
+cp -rf /home/aravinth/aracreate.com/repo/* /home/aravinth/aracreate.com/html/
