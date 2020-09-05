5de0344a0a5eea56e84471f7a2bbd26b37be3f36
Aravinth Panch <me@aravinth.info>
2020-09-05 15:43:25 +0200

    Add visualmakers.dreamspace.academy

--- host-dreamspace-academy.js
+++ host-dreamspace-academy.js
@@ -72,3 +72,17 @@ plan.target('corona.dreamspace.academy', {
   git_src_dir: '',
   webhook_dir: '/var/www/webhook/',
 });
+
+plan.target('visualmakers.dreamspace.academy', {
+  host: 'dreamspace.academy',
+  username: 'root',
+  privateKey: '/Users/aravinth/.ssh/id_rsa',
+  agent: process.env.SSH_AUTH_SOCK,
+
+  domain_name: 'visualmakers.dreamspace.academy',
+  port_number: 80,
+  git_repo: 'git@github.com:dreamspace-academy/dreamspace.academy.git',
+  git_branch: 'master',
+  git_src_dir: '',
+  webhook_dir: '/var/www/webhook/',
+});
