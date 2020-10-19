38b29c28f1f233b56c74258c47b70c88165471b3
Aravinth Panch <me@aravinth.info>
2020-10-19 21:29:13 +0200

    Update script for url redirect

--- src/create-redirect.js
+++ src/create-redirect.js
@@ -12,10 +12,9 @@ plan.remote("create-redirect", function (remote) {
 
   // definitions
   var $ = remote.runtime;
-
-  var apache2_conf_file = config.apache2_conf_dir + $.domain_name + ".conf";
+  var apache2_conf_file = config.apache2_conf_dir + $.domain_name_reversed + ".conf";
   var virtual_host_str = "<VirtualHost *:" + config.http_port_number + ">";
-  var server_admin_str = "ServerAdmin me@aravinth.info";
+  var server_admin_str = "ServerAdmin " + config.host;
   var server_name_str = "ServerName " + $.domain_name;
   var server_alias_str = "ServerAlias www." + $.domain_name;
   var redirect_url_str = "RedirectPermanent / " + $.redirect_url;
@@ -33,9 +32,8 @@ plan.remote("create-redirect", function (remote) {
 
   //enable apache site
   remote.with("cd " + config.apache2_conf_dir, function () {
-    remote.exec("a2ensite " + $.domain_name + ".conf");
+    remote.exec("a2ensite " + $.domain_name_reversed + ".conf");
   });
   remote.exec("apache2ctl configtest");
-  remote.exec("systemctl reload apache2");
   remote.exec("systemctl restart apache2");
 });
