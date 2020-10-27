fe7ed0f05cb65aa40ca9cb61731174485b73e058
Aravinth Panch <me@aravinth.info>
2020-10-28 00:58:58 +0100

    Add arametrics-test-app host

--- hosts/host-apps.js
+++ hosts/host-apps.js
@@ -12,7 +12,20 @@ plan.target("arametrics-clockify-google", {
   git_repo: "git@github.com:AravinthPanch/araMetrics.git",
   git_branch: "master",
   git_src_dir: "arametrics-clockify-google/",
-  local_src_dir:
-    "/Users/aravinth/local-workspace/araMetrics/arametrics-clockify-google/",
-  local_secrets_dir: "secrets/",
+  local_src_dir: "/Users/aravinth/local-workspace/-araCreate-git-repos/araMetrics/arametrics-clockify-google/",
+  local_secrets_dir: "secrets/"
+});
+
+plan.target("arametrics-test-app", {
+  host: config.host,
+  username: config.username,
+  privateKey: config.privateKey,
+  agent: config.agent,
+
+  app_name: "arametrics-test-app",
+  git_repo: "git@github.com:AravinthPanch/araMetrics.git",
+  git_branch: "master",
+  git_src_dir: "arametrics-test-app/",
+  local_src_dir: "/Users/aravinth/local-workspace/-araCreate-git-repos/araMetrics/arametrics-test-app/",
+  local_secrets_dir: "secrets/"
 });
