6a1f0780607ad0691300265e8d5886f2e185e0b6
Aravinth Panch <me@aravinth.info>
2020-10-28 00:59:11 +0100

    Update app creation script and add todos

--- src/create-app.js
+++ src/create-app.js
@@ -3,7 +3,14 @@
  * Author:        Aravinth Panch
  */
 
-// TODO: install command as scripts arametrics-clockify-google_install.sh
+// TODO: cronjob placement based on host input or have the app always running in supervisorctl and have tasker insider
+// TODO: supervisorctl placement based on input
+// TODO: send aracloud_root,app_name,app_root as commandline arguments to install, run scripts
+// TODO: webhook deployment
+// TODO: clean code, add comments,update headers,update readme
+// TODO: control scp file and change ownerships
+// TODO: add secret to webhook
+// TODO: add log and activity monitor
 
 var plan = require("flightplan");
 var config = require("../config/config");
@@ -19,8 +26,12 @@ plan.remote("create-app", function (remote) {
   // definitions
   var $ = remote.runtime;
   app_root = config.apps_root + $.app_name + "/";
-  app_dir = app_root + "app/";
   app_repo_dir = app_root + "repo/";
+  app_dir = app_root + "app/";
+  app_config_dir = app_dir + "config/";
+  app_install_script = $.app_name + "-install.sh";
+  app_run_script = $.app_name + ".sh";
+  app_cron_script = $.app_name + ".cron";
 
   //Create app folder with necessary folders
   remote.rm("-rf " + app_root);
@@ -33,19 +44,21 @@ plan.remote("create-app", function (remote) {
   remote.with("cd " + app_repo_dir, function () {
     remote.git("submodule update --init");
   });
+
   remote.cp("-r " + app_repo_dir + $.git_src_dir + "* " + app_dir);
 
   // place cronjob
-  remote.cp(app_dir + "config/" + $.app_name + ".cron" + " " + config.cron_dir);
+  remote.cp(app_config_dir + app_cron_script + " " + config.cron_dir);
   remote.with("cd " + config.cron_dir, function () {
-    remote.exec("crontab " + $.app_name + ".cron");
+    remote.exec("crontab " + app_cron_script);
   });
 
   // install depencies
-  remote.with("cd " + app_dir, function () {
-    remote.exec("pip install -r requirements.txt");
+  remote.with("cd " + app_config_dir, function () {
+    remote.chmod("+x " + app_install_script);
+    remote.chmod("+x " + app_run_script);
+    remote.exec("./" + app_install_script);
   });
-
 });
 
 // Transfer necessary files from local to remote
